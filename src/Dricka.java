702990e47ca3f751816e8666aad0430cb8b0513a
Kalle Nilsson <domare2@hotmail.com>
2021-03-05 03:00:45 +0100

    Description metoden är nu fullständig i Dricka klassen

--- src/Dricka.java
+++ src/Dricka.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,14 +12,24 @@
  * @author Kalle
  */
 public class Dricka extends Varor implements VarorFunktioner{
+    //scanner som fungerar i hela klassen
+   Scanner scan = new Scanner(System.in);
+   //2 instanser av Dricka
+   public static Dricka cola = new Dricka(10, "cola", "söt");
+   public static Dricka fanta = new Dricka(10, "fanta", "surt");
     
     public Dricka(int kostnad, String namn, String beskrivning) {
         super(kostnad, namn, beskrivning);
     }
 
     @Override
+    
     public void Description() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println(namn + " Är en väldigt " + beskrivning +" dryck som kostar " + kostnad );
+        System.out.println("Vill du gå vidare till menyn för att köpa drycken?");
+        //kallar på Buy metoden för att säkerställa köpet
+        Buy();
+    
     }
 
     @Override
@@ -26,7 +39,9 @@ public class Dricka extends Varor implements VarorFunktioner{
 
     @Override
     public void Use() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        System.out.println("Du drack upp en 50cl " + namn);
+    
     }
     
 }
