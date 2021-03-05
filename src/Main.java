5731d27415673cd9136d3aadc33ab312d09584d6
Kalle Nilsson <domare2@hotmail.com>
2021-03-05 02:37:18 +0100

    Implementerat en while loop och en metod till menyvalet

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,21 @@
  * @author Kalle
  */
 public class Main {
+    //Utanför en metod och gjord till static för att andra klasser ska kunna ändra värdet på "setIt"
+    
+    public static boolean setIt = true;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hej");
+        System.out.println("Hej och välkommen till Kalles automat!");
+        
+        //while loopen ser till att menyn repeteras tills det att setIt blir false
+        while (setIt){
+        //anropar metoden som innehåller alla standard menyn
+        Meny.standardMeny();
     }
-    
+    }
 }
